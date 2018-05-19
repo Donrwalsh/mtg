8921f38739a1261be7a4c60d593a258936b519f4
Don R Walsh <donrwalsh@gmail.com>
2018-05-19 11:35:58 -0500

    Quick take on displaying results on the API page
    Putting some effort into getting them human readable makes sense with the spirit of this project. Dropped some considerations in the readme for next steps in this area.

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 mtg
+
+* Capturing of the variations information isn't doing what I want it to.
+* Need to come up with some way to display multiple cards cleanly on the API page.
+* I want the left pane to scroll separately from the right pane on APIs.
+* The javascript is sort of a mess. Could use a better way of organizing than dropping stuff right into index.js. 
--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,3 +1,31 @@
+function display_card_val(field_name, type, value) {
+    switch(type) {
+        case 'int':
+            return "<strong>" + field_name + "</strong>: " + value + ",<br>";
+        case 'string':
+            return "<strong>" + field_name + "</strong>: \"" + value + "\",<br>"
+        case 'multi-string':
+            var result = "<strong>" + field_name + "</strong>: [";
+            $.each(value, function (i, item) {
+                if (i === (value).length-1) {
+                    result += "\"" + item + "\"";
+                } else {
+                    result += "\"" + item + "\",";
+                }
+            });
+            return result + "],<br>";
+        case 'multi-int':
+            var result = "<strong>" + field_name + "</strong>: [";
+            $.each(value, function (i, item) {
+                if (i === (value).length-1) {
+                    result += item;
+                } else {
+                    result += "" + item + ",";
+                }
+            });
+            return result + "],<br>";
+    }
+}
 
 $(document).ready(function () {
 
@@ -13,18 +41,42 @@ $(document).ready(function () {
                 var resJSON = "";
                 console.log(response)
                 $.each(response, function (i, response) {
-                    // resJSON += "id: " + response.id + "<br>" +
-                    // "name: " + response.name + "<br>";
-                    // if ( response.names.length > 0) {
-                    //     resJSON += "names: " + response.names + "<br>";
-                    // }
-                    resJSON += JSON.stringify(response) + "<br>";
+                    resJSON += "<p>";
+                    resJSON += display_card_val('id', 'int', response.id);
+                    resJSON += display_card_val('name', 'string', response.name);
+                    resJSON += display_card_val('names', 'multi-string', response.names);
+                    resJSON += display_card_val('colors', 'multi-string', response.colors);
+                    resJSON += display_card_val('colorIdentity', 'multi-string', response.colorIdentity);
+                    resJSON += display_card_val('manaCost', 'string', response.manaCost);
+                    resJSON += display_card_val('cmc', 'int', response.cmc);
+                    resJSON += display_card_val('set', 'int', response.set);
+                    resJSON += display_card_val('rarity', 'string', response.rarity);
+                    resJSON += display_card_val('text', 'string', response.text);
+                    resJSON += display_card_val('flavor', 'string', response.flavor);
+                    resJSON += display_card_val('supertypes', 'multi-string', response.supertypes);
+                    resJSON += display_card_val('types', 'multi-string', response.types);
+                    resJSON += display_card_val('subtypes', 'multi-string', response.subtypes);
+                    resJSON += display_card_val('variations', 'multi-int', response.variations);
+                    resJSON += display_card_val('artist', 'string', response.artist);
+                    resJSON += display_card_val('number', 'string', response.number);
+                    resJSON += display_card_val('power', 'string', response.power);
+                    resJSON += display_card_val('toughness', 'string', response.toughness);
+                    resJSON += display_card_val('loyalty', 'int', response.loyalty);
+                    resJSON += display_card_val('multiverseid', 'int', response.multiverseid);
+                    resJSON += display_card_val('watermark', 'string', response.watermark);
+                    resJSON += display_card_val('border', 'string', response.border);
+                    resJSON += display_card_val('layout', 'string', response.layout);
+                    resJSON += display_card_val('timeshifted', 'string', response.timeshifted);
+                    resJSON += display_card_val('reserved', 'string', response.reserved);
+                    resJSON += display_card_val('starter', 'string', response.starter);
+                    resJSON += "</p>";
+                    // resJSON += JSON.stringify(response) + "<br>";
                 });
                 if( resJSON == "") {
                     resJSON = "{Empty Response}";
                 }
-                $('#rCard').empty();
-                $('#rCard').append(resJSON);
+                $('#output').empty();
+                $('#output').append(resJSON);
             },
             error: function (errmsg, txtstatus) {
                 var msg = JSON.parse(errmsg.responseText).message;
@@ -50,8 +102,8 @@ $(document).ready(function () {
                 if( resJSON == "") {
                     resJSON = "{Empty Response}";
                 }
-                $('#rCards').empty();
-                $('#rCards').append(resJSON);
+                $('#output').empty();
+                $('#output').append(resJSON);
             },
             error: function (errmsg, txtstatus) {
                 var msg = JSON.parse(errmsg.responseText).message;
