1ab008b573885900946af318d518d0e213a5e071
Don R Walsh <donrwalsh@gmail.com>
2018-05-26 23:56:02 -0500

    Image asset gathering
    Downloaded a majority chunk of the images with this code. 'Unique' frames and other complicating factors are omitted for now and everything's built in main.py and will need to move to a service

--- builder/main.py
+++ builder/main.py
@@ -8,10 +8,21 @@ import pprint
 import os
 
 # Command-Line invocations
+from writer_service import Writer
+
 parser = argparse.ArgumentParser()
+
+parser.add_argument("--verify", help="Verifies nothing, but will", action="store_true")
 parser.add_argument("--build", help="destroy, then build the database", action="store_true")
 parser.add_argument("--images", help="fetch images (in-progress", action="store_true")
+
 args = parser.parse_args()
+
+# if args.verify:
+# Verify the MTGJSON data is the latest release.
+# Verify the Databsae (in tiers):
+# Verify the number of cards matches mtgJSON data.
+
 
 JsonService = source.json_service.JsonService('source')
 DatabaseService = database.database_service.DatabaseService()
@@ -25,22 +36,30 @@ SETS = []
 for set in DATA_SETS:
     SETS.append(set['code'])
 
+
+
 if args.build:
     DatabaseService.build_database(DATA_CARDS, DATA_SETS)
 
 if args.images:
-    DatabaseService.query("SELECT cards.id, cards.multiverseid, `sets`.code FROM `sets` INNER JOIN cards ON "
+    DatabaseService.query("SELECT cards.id, cards.multiverseid, `sets`.code, cards.layout FROM `sets` INNER JOIN cards ON "
                           "sets.id=cards.`set`")
     id_map = DatabaseService.cur.fetchall()
     for trip in id_map:
-        if trip[1] is not 'null':
-            r = requests.get(url='https://api.scryfall.com/cards/multiverse/' + str(trip[1]))
-            print("Downloading " + r.name + " from " + r.set_name)
-            pprint.pprint(r)
-            exit()
-            url = r.json()['image_uris']['png']
-            response = requests.get(url, stream=True)
-            if not os.path.exists('../' + trip[2] + '/'):
-                os.makedirs('../' + trip[2] + '/')
-            with open('../images/' + trip[2] + '/' + str(trip[0]) + '.png', 'wb') as out_file:
-                shutil.copyfileobj(response.raw, out_file)
+        # Skipped certain cards due to complications to sort out later.
+        if not trip[2] in ('VAN') and trip[3] != "token" and trip[3] != 'split' and trip[3] != 'double-faced':
+            if trip[1] is not None:
+                pprint.pprint("Current Trip: " + str(trip))
+                if not os.path.exists('../images/' + trip[2] + '/'):
+                    os.makedirs('../images/' + trip[2] + '/')
+                if not os.path.exists('../images/' + trip[2] + '/' + str(trip[0]) + '.jpg'):
+                    try:
+                        r = requests.get(url='https://api.scryfall.com/cards/multiverse/' + str(trip[1]))
+                        url = r.json()['image_uris']['normal']
+                        response = requests.get(url, stream=True)
+                        with open('../images/' + trip[2] + '/' + str(trip[0]) + '.jpg', 'wb') as out_file:
+                            shutil.copyfileobj(response.raw, out_file)
+                    except KeyError as e:
+                        Writer.error("An error occurred when retrieving image:", e)
+                        # The following trip causes an error. Not sure why yet.
+                        # 'image_uris'"Current Trip: (35679, 442789, 'DDU', 'normal')"
